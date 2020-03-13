115d0a62ce23b089e8dca7b55b851ab0765a43e6
StrappedGlint13 <matiasbrax@gmail.com>
2020-03-13 22:53:02 +0200

    tasks

---
+++ application/tasks/__init__.py
@@ -0,0 +1,14 @@
+── application
+│   ├── __init__.py
+│   ├── tasks
+│   │   └── __init__.py
+│   ├── templates
+│   │   ├── index.html
+│   │   └── layout.html
+│   ├── views.py
+├── run.py
+├── .gitignore
+├── Procfile
+├── requirements.txt
+├── README.md
+...
